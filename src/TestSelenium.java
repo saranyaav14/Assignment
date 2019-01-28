33e9eac5ed2fafa3e1c829e2ffaeaf2ccdcc4694
ubuntu <saranyaav94@yahoo.in>
2019-01-28 02:21:12 -0800

    Assignment

--- src/TestSelenium.java
+++ src/TestSelenium.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 
 
 public class TestSelenium {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
         String driverPath = "/home/ubuntu/Downloads/chromedriver";
         String elementOne = "Checkboxes";
@@ -15,20 +15,19 @@ public class TestSelenium {
         String url = "https://the-internet.herokuapp.com";
 
         System.setProperty("webdriver.chrome.driver", driverPath);
-        WebDriver driver=new ChromeDriver();
+        WebDriver driver = new ChromeDriver();
         driver.get(url);
         ArrayList<String> elements = new ArrayList<>();
         elements.add(elementOne);
         elements.add(elementTwo);
-        System.out.println(elements.size());
-        for (int i = 0; i<=elements.size(); i=i+1) {
+        for (int i = 0; i < elements.size(); i = i + 1) {
             try {
-                driver.findElement(By.xpath(String.format("//a[contains(.,'%1$s')]", elements.get(i))));
-                System.out.println("Element_is_present");
-            }
-
-            catch(org.openqa.selenium.NoSuchElementException e) {
-                System.out.println("Element_is_not_present");
+                WebElement link = driver.findElement(By.xpath(String.format("//a[contains(.,'%1$s')]", elements.get(i))));
+                link.click();
+                driver.navigate().back();
+                System.out.println(elements.get(i)+" Link is present");
+            } catch (org.openqa.selenium.NoSuchElementException e) {
+                System.out.println(" Link's name not found");
             }
 
 
